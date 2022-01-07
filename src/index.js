9f33243b1e49376b815d00621aa03abdabeb185f
flavio manzotti <manzottiflavio22@gmail.com>
2022-01-07 20:16:47 -0300

    criando middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ app.use(express.json());
 
 const customers=[];
 
+//middleware
+function verifyIfExistsAccountCpf(request,response,next){
+    const {cpf}= request.headers;
+    //const {cpf} = request.params;
+
+const customer = customers.find((customer)=>customer.cpf === cpf);
+
+
+if(!customer){
+    return response.status(400).json({error:"customer not found"});
+}
+request.customer=customer;
+
+
+return next();
+
+
+}
+
+
+
+
+
 app.post("/account",(request,response)=>{
 const {cpf, name} = request.body;
 
@@ -28,15 +51,8 @@ return response.status(201).send();
 
 });
 
-app.get("/statement", (request,response)=>{
-const {cpf}= request.headers;
-    //const {cpf} = request.params;
-
-const customer = customers.find((customer)=>customer.cpf === cpf);
-
-if(!customer){
-    return response.status(400).json({error:"customer not found"});
-}
+app.get("/statement",verifyIfExistsAccountCpf, (request,response)=>{
+const {customer}=request;
 
 return response.status(200).json(customer.statement);
 
